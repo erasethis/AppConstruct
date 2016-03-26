d91d89dacb94353ec519b05b7f1ebf62a962d48c
Thorsten Westheider <thorsten.westheider@outlook.com>
2016-03-26 22:00:54 +0100

    Working example - sidebar collapsing and expanding

--- app/sidebar.component.js
+++ app/sidebar.component.js
@@ -34,7 +34,9 @@ System.register(['angular2/core', './sidebar.service'], function(exports_1, cont
                 Sidebar = __decorate([
                     core_1.Component({
                         selector: "x-sidebar",
-                        template: "\n        <div>\n        </div>\n        "
+                        template: "\n        <div [ngClass]=\"{collapsed: collapsed}\">\n            <ul>\n                <li>Menu Item 1</li>\n                <li>Menu Item 2</li>\n                <li>Menu Item 3</li>\n                <li>Menu Item 4</li>\n                <li>Menu Item 5</li>\n            </ul>\n        </div>\n        ",
+                        styles: ["\n        div {\n            width: 180px;\n            transition: all 0.25s ease-in-out;\n        }\n        div.collapsed {\n            width: 0;\n            transition: all 0.25s ease-in-out;\n        }\n        div ul {\n            list-style: none;\n            color: #eee;\n            padding: 14px;\n            margin: 0;\n        }   \n        div ul li {\n            white-space: nowrap;\n            margin-left: 0;\n            transition: all 0.25s ease-in-out;\n        }\n        div.collapsed ul li {\n            white-space: nowrap;\n            margin-left: -180px;\n            transition: all 0.25s ease-in-out;\n        }\n        "
+                        ]
                     }), 
                     __metadata('design:paramtypes', [sidebar_service_1.SidebarService])
                 ], Sidebar);
--- app/sidebar.component.ts
+++ app/sidebar.component.ts
@@ -4,9 +4,43 @@ import { SidebarService } from './sidebar.service';
 @Component({
     selector: "x-sidebar",
     template: `
-        <div>
+        <div [ngClass]="{collapsed: collapsed}">
+            <ul>
+                <li>Menu Item 1</li>
+                <li>Menu Item 2</li>
+                <li>Menu Item 3</li>
+                <li>Menu Item 4</li>
+                <li>Menu Item 5</li>
+            </ul>
         </div>
+        `,
+    styles: [`
+        div {
+            width: 180px;
+            transition: all 0.25s ease-in-out;
+        }
+        div.collapsed {
+            width: 0;
+            transition: all 0.25s ease-in-out;
+        }
+        div ul {
+            list-style: none;
+            color: #eee;
+            padding: 14px;
+            margin: 0;
+        }   
+        div ul li {
+            white-space: nowrap;
+            margin-left: 0;
+            transition: all 0.25s ease-in-out;
+        }
+        div.collapsed ul li {
+            white-space: nowrap;
+            margin-left: -180px;
+            transition: all 0.25s ease-in-out;
+        }
         `
+    ]
 })
 
 export class Sidebar {
